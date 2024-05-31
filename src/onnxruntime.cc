80bd17d20d4d1fe996a9aab6a04eace77b58c571
Gyula Zakor <gyula.zakor@htecgroup.com>
2024-05-31 14:13:14 +0000

    Revert "Force GPU kind for modelinstance as well"
    This reverts commit f40b8322bf59e449a74f36aff6f53e39d9888bad.

--- src/onnxruntime.cc
+++ src/onnxruntime.cc
@@ -3069,9 +3069,8 @@ TRITONBACKEND_ModelInstanceInitialize(TRITONBACKEND_ModelInstance* instance)
 
   int32_t device_id;
   RETURN_IF_ERROR(TRITONBACKEND_ModelInstanceDeviceId(instance, &device_id));
-  // Force GPU kind for modelinstance
-  TRITONSERVER_InstanceGroupKind kind = TRITONSERVER_InstanceGroupKind::TRITONSERVER_INSTANCEGROUPKIND_GPU;
-  // RETURN_IF_ERROR(TRITONBACKEND_ModelInstanceKind(instance, &kind));
+  TRITONSERVER_InstanceGroupKind kind;
+  RETURN_IF_ERROR(TRITONBACKEND_ModelInstanceKind(instance, &kind));
 
   LOG_MESSAGE(
       TRITONSERVER_LOG_INFO,
